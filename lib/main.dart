4298794a08519ca00343d6d804e29e907bc9aaf4
AntonDeda1 <antondeda17@outlook.com>
2023-08-30 13:53:24 +0200

    Added Skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:expenses/expenses.dart';
 
 void main() {
-  runApp(const MyApp(
-    
+  runApp(MaterialApp(
+    home: Expenses(),
   ));
 }
